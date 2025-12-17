cd972663f652e4898e3b392c371b7571521f8fe9
otmbneto <otmbneto@protonmail.com>
2025-12-17 15:44:44 -0300

    Added CORS to app

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(items.router)
 app.include_router(categories.router)
